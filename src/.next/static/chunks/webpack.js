36ea0795cd2150c48d5bc2fb95455b6fec1a6768
Yu-Gi-Oh-Platform OzG71LXXeKVjzhP8LLWaYM4bo <177896519+OzG71LXXeKVjzhP8LLWaYM4bo@users.noreply.github.com>
2024-09-15 19:20:06 +1000

    a

--- src/.next/static/chunks/webpack.js
+++ src/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bd9b1ca76425460b"; }
+/******/ 		__webpack_require__.h = function() { return "ae08aafca8448c30"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,14 +1,27 @@
+'use client';
 
-import React from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
 
-const page = () => {
+const SignUpPage = () => {
+
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
+  const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
   return (
-    <div className="flex-grow flex flex-col">
-        <div className="flex justify-center items-center min-h-screen">
-            hi
-        </div>
+    <div className="flex justify-center items-center min-h-screen">
+      <Auth 
+        supabaseClient={supabase} 
+        appearance={{ theme: ThemeSupa }} 
+        providers={['google', 'github']} // Add any OAuth providers you'd like to use
+        view="sign_up" // This explicitly sets the Auth view to Sign-Up
+      />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default SignUpPage;
